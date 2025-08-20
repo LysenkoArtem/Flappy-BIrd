67c7cbabc47a43aca9c7b414516e450ec8c7c697
Lysenko Artyom <lysenkoartyom@MacBook-Air-Lysenko.local>
2025-08-20 14:26:45 +0500

    Commit changed files

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Flappy Bird");
 
-        frame.add(new Window());
+        Window window = new Window();
+        frame.add(window);
 
+        window.startThread();
 
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,79 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-class Window extends JPanel {
+class Window extends JPanel implements Runnable{
     int width = 800;
     int height = 600;
+
+    int FPS = 40;
+
+
+    Thread thread;
+
+    Bird bird = new Bird();
+
+    KeyboardListner kbl = new KeyboardListner();
+
 
     public Window() {
         this.setPreferredSize(new Dimension(width, height));
         this.setDoubleBuffered(true);
         this.setBackground(new Color(110, 180, 240));
+
+        this.addKeyListener(kbl);
+        this.setFocusable(true);
+
     }
+
+    public void startThread() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        bird.paint(g2);
+
+    }
+
+    private void update() {
+        if (kbl.is_pressed) {
+            bird.jump();
+            kbl.is_pressed = false;
+        }
+        bird.update();
+    }
+
+	@Override
+	public void run() {
+        double interval = (double) 1000000000 / FPS;
+        double delta = 0;
+        long curTime;
+        long lastTime = System.nanoTime();
+
+        while (thread != null) {
+            curTime = System.nanoTime();
+
+            delta += (curTime - lastTime) / interval;
+            lastTime = curTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+
+        }
+
+	}
+
+    
 
 }
